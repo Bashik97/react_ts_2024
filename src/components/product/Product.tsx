9e8fa9f22cc08eb790df1595d60ff7d149f8f01a
Bashikdev <bashikdev@gmail.com>
2024-10-21 00:14:19 +0300

    hw1

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -5,8 +5,10 @@ type TypeProps = { item: IProductModel };
 const Product: FC<TypeProps> = ({item}) => {
     return (
         <div>
-                <h3>{item.title}</h3>
+            <h3>{item.title}</h3>
             <img src={item.thumbnail} alt={item.title}/>
+            <h2>{item.shippingInformation}</h2>
+            <h2>{item.tags}</h2>
         </div>
     );
 };
